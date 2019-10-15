4f6c84f6facb7badb10cdcfb3d7d88cef2e155ee
Brandon Pittman <brandon@brandonpittman.net>
2019-10-15 12:34:07 +0900

    Add support for ts,tsx,jsx files along with composables

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,43 +1,46 @@
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
+const fs = require('fs')
 
-export default function(Vue) {
-  try {
-    const requireComponent = require.context(
-      // The relative path of the components folder
-      '../../src/composables',
-      // Whether or not to look in subfolders
-      true,
-      // The regular expression used to match base component filenames
-      /use-[a-z]\w+\.(js|ts)$/,
-    )
-
-    requireComponent.keys().forEach(fileName => {
-      // Get component config
-      const componentConfig = requireComponent(fileName)
-
-      // Get PascalCase name of component
-      const componentName = upperFirst(
-        camelCase(
-          // Gets the file name regardless of folder depth
-          fileName
-            .split('/')
-            .pop()
-            .replace(/\.\w+$/, ''),
-        ),
+export default function (Vue) {
+  if (fs.existsSync('../../src/composables')) {
+    try {
+      const requireComponent = require.context(
+        // The relative path of the components folder
+        '../../src/composables',
+        // Whether or not to look in subfolders
+        true,
+        // The regular expression used to match base component filenames
+        /use-[a-z]\w+\.(js|ts)$/,
       )
 
-      // Register component globally
-      Vue.component(
-        componentName,
-        // Look for the component options on `.default`, which will
-        // exist if the component was exported with `export default`,
-        // otherwise fall back to module's root.
-        componentConfig.default || componentConfig,
-      )
-    })
-  } catch (e) {
-    console.error(e)
+      requireComponent.keys().forEach(fileName => {
+        // Get component config
+        const componentConfig = requireComponent(fileName)
+
+        // Get PascalCase name of component
+        const componentName = upperFirst(
+          camelCase(
+            // Gets the file name regardless of folder depth
+            fileName
+              .split('/')
+              .pop()
+              .replace(/\.\w+$/, ''),
+          ),
+        )
+
+        // Register component globally
+        Vue.component(
+          componentName,
+          // Look for the component options on `.default`, which will
+          // exist if the component was exported with `export default`,
+          // otherwise fall back to module's root.
+          componentConfig.default || componentConfig,
+        )
+      })
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   try {
