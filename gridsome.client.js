af7bc1fba1eeceeb53581fb2568cf0fd147231ba
Brandon Pittman <brandon@brandonpittman.net>
2019-10-15 12:29:28 +0900

    Add support for ts,tsx,jsx files along with composables

--- gridsome.client.js
+++ gridsome.client.js
@@ -5,11 +5,49 @@ export default function(Vue) {
   try {
     const requireComponent = require.context(
       // The relative path of the components folder
+      '../../src/composables',
+      // Whether or not to look in subfolders
+      true,
+      // The regular expression used to match base component filenames
+      /use-[a-z]\w+\.(js|ts)$/,
+    )
+
+    requireComponent.keys().forEach(fileName => {
+      // Get component config
+      const componentConfig = requireComponent(fileName)
+
+      // Get PascalCase name of component
+      const componentName = upperFirst(
+        camelCase(
+          // Gets the file name regardless of folder depth
+          fileName
+            .split('/')
+            .pop()
+            .replace(/\.\w+$/, ''),
+        ),
+      )
+
+      // Register component globally
+      Vue.component(
+        componentName,
+        // Look for the component options on `.default`, which will
+        // exist if the component was exported with `export default`,
+        // otherwise fall back to module's root.
+        componentConfig.default || componentConfig,
+      )
+    })
+  } catch (e) {
+    console.error(e)
+  }
+
+  try {
+    const requireComponent = require.context(
+      // The relative path of the components folder
       '../../src/components',
       // Whether or not to look in subfolders
       true,
       // The regular expression used to match base component filenames
-      /(App|Base|V)[A-Z]\w+\.(vue|js)$/,
+      /(App|Base|V)[A-Z]\w+\.(vue|js|ts|jsx|tsx)$/,
     )
 
     requireComponent.keys().forEach(fileName => {
